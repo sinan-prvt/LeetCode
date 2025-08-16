acf6ca2a22416b4d81238f6734274e2547c1c8fa
Mohamed Sinan <mohamedsinan9400@gmail.com>
2025-08-16 10:03:05 +0530

    Time: 1 ms (24.27%), Space: 55 MB (97.33%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {   
+    if(n < 0 ) return false
+    if((n & (n - 1)) !== 0) return false
+    return(n & 0x55555555) !== 0 ;
+};
