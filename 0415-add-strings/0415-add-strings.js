13deff9d09b95cf70a5a6526cc7bc3ae185e714e
Mohamed Sinan <mohamedsinan9400@gmail.com>
2025-09-28 11:11:54 +0530

    Time: 2 ms (70.13%), Space: 55.6 MB (70.84%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var addStrings = function(num1, num2) {
+    let i = num1.length - 1
+    let j = num2.length - 1
+    let carry = 0
+    let res = ""
+    
+    while(i >= 0 || j >= 0 || carry) {
+        let d1 = i >= 0 ? num1[i] - '0' : 0
+        let d2 = j >= 0 ? num2[j] - '0' : 0
+
+        let sum = d1 + d2 + carry
+        res = (sum % 10) + res 
+        carry = Math.floor(sum / 10)
+
+        i--
+        j--
+    }
+    return res
+};
